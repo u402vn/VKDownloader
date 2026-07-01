c11506e3918fa543f7f994409f3fd8187c177d57
Волков Кирилл <kirill.volkov@fors.ru>
2026-07-01 23:23:38 +0300

    New token

--- vk_auth.py
+++ vk_auth.py
@@ -1,14 +1,14 @@
 ﻿VKTokens = [
 
-#6776474
-'vk1.a.sHvmwGEd1XzFnrO4GmUV9Pac4_NLQnp4Rl5oM6UgM-omQliFVICzxcEKxRFLl-rfosoD6hyO7kjp3b0IAsTkd18pC6NK5kUFi-LpQXSAkaEYnVJ1CgPagD89zB2j9giYB7xgKOsnv6ulqgBJTDvByIoqNknobGtVF31wdV0Rgl5Sncm0vWjSlmLvB-xsooo56yIlUggTp-Wmf5aKwcgGzQ',
-
 #10570017
-'vk1.a.ykwyaiQsWgT6-WKDMWcd_cdMzXEywlFHdQCFSPdzdikB0zraerT_zxfuUEwSSM4FVWC4frkyTaVP8CBEACedJAHAzzl09cH4sQl_QsmiTo7F2VJWRwYdy660_XoTKMs3-KnIZWT1R1-B-JJ63RY2ev7rZCbBPCS9Ub-h4qFOqNJRPg3EwZ-hoImcNIlErt-0fBnaGMAXSkDsL20pob1g8g',
+'vk1.a.Mgd9IGeN5eejV-dluHV0DVlsKtE0yq-jhBNKklegnBsb8Ln_EH5gj4DzeDANHZAXCBK2FZQM278AA1NpOskNaYM1ZOgim9HbvGhO6_9ZpfTF6s7tdY0eR0iCxMM8SkBeeEIKrepx0c9mOHijMd-ZLDnQEwQBDkiGjtU0vZIb7RSA2wTi1OBstjB4i5fcqaRJp5C5kyWDuDlm2T4peyA77w',
+
+#6776474
+#'vk1.a.ykwyaiQsWgT6-WKDMWcd_cdMzXEywlFHdQCFSPdzdikB0zraerT_zxfuUEwSSM4FVWC4frkyTaVP8CBEACedJAHAzzl09cH4sQl_QsmiTo7F2VJWRwYdy660_XoTKMs3-KnIZWT1R1-B-JJ63RY2ev7rZCbBPCS9Ub-h4qFOqNJRPg3EwZ-hoImcNIlErt-0fBnaGMAXSkDsL20pob1g8g',
 
 ]
 
-
+# Зайти на https://vkhost.github.io
 
 
 DatabaseConnectionString = "host=localhost dbname=vk user=postgres password=masterkey port=5432"
